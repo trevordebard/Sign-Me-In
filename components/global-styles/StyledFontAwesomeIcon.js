4bc9c7863a57bfb7c75d4b7b7724ab3a842d2d91
Trevor DeBardeleben <trevordebard@gmail.com>
2021-02-07 13:34:38 -0500

    Update packages

--- components/global-styles/StyledFontAwesomeIcon.js
+++ components/global-styles/StyledFontAwesomeIcon.js
@@ -2,15 +2,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styled from 'styled-components';
 
 export default styled(FontAwesomeIcon)`
+  width: 1.5rem;
   color: ${props =>
     props.colors && props.colors.default
       ? props.colors.default
       : props.theme.primary};
   &:hover {
     color: ${props =>
-      props.colors && props.colors.hover
-        ? props.colors.hover
-        : props.theme.primary_light};
+    props.colors && props.colors.hover
+      ? props.colors.hover
+      : props.theme.primary_light};
     cursor: pointer;
   }
 `;
