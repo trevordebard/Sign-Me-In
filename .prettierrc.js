9d060877ddf8d7fef4a031a336a7a4829cbdb985
Trevor DeBardeleben <trevordebard@gmail.com>
2021-02-08 20:58:48 -0500

    Setup prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 80,
+  //below line only for windows users facing CLRF and eslint/prettier error
+  // non windows users feel free to delete it
+  endOfLine: 'auto',
+  arrowParens: 'avoid',
+}
